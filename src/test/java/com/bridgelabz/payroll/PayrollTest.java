be78f3dac4ab92f863f604578b9998a06a89341d
Archanagowda23 <archanar249@gmail.com>
2022-04-11 21:18:42 +0530

    Added method to read the file

---
+++ src/test/java/com/bridgelabz/payroll/PayrollTest.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.payroll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+@SuppressWarnings("deprecation")
+public class PayrollTest {
+
+    @Test
+    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeesEntries() {
+        EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, 25000.56, "Devnandan Kumar"),
+                new EmployeePayrollData(2, 15000.65, "Manoj Kumar"),
+                new EmployeePayrollData(3, 22000.67, "Abhishek sinha") };
+        EmployeePayrollService EmployeePayrollService;
+        EmployeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+        EmployeePayrollService.write(com.bridgelabz.payroll.EmployeePayrollService.IOService.FILE_IO);
+        long entriesCount=EmployeePayrollService.countEntries(com.bridgelabz.payroll.EmployeePayrollService.IOService.FILE_IO);
+        EmployeePayrollService.printData(com.bridgelabz.payroll.EmployeePayrollService.IOService.FILE_IO);
+        Assertions.assertEquals(3, entriesCount);
+    }
+
+    @Test
+    public void givenFileOnReadingFromFileShouldMatchEmployeeCount() {
+        EmployeePayrollService payrollService=new EmployeePayrollService();
+        long entries=payrollService.readEmployeeData(EmployeePayrollService.IOService.FILE_IO);
+    }
+
+}
